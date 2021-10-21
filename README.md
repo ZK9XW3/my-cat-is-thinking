5d7e35fbe9a38cf0c35165659025092ee414aff7
ZK9XW3 <peteralexandremusic@gmail.com>
2021-10-21 12:31:23 +0200

    feat(readme): added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# What is my cat thinking ?
+## Description
+Ever wondered what was your cat thoughts ? Here is the answer !!!
+Seriously it is only a small project wich is meant to make me discover how easy API platform is... 
+With this project i'm working with my beloved Symfony on API, JWT, and unit test with postman. If i can i'd like to create an integration pipeline with newman and github actions to learn how to work with CI. Might have to use docker for this... we'll see.
+Front-End side i'm gonna start simple maybe with bootstrap, webpack encore, or just flex still not sure yet.
+
+PS: I'm tryin to make a well documented project so i get use to be a good collegue !!!
+
+## Installation
+- composer install
+- set your .env.local
+- doctrine:database:create
+- doctrine:migrations:migrate
---
+++ src/Entity/Thought.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ThoughtRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ThoughtRepository::class)
+ */
+class Thought
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
